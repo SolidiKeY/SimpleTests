8f0ca5186a7aa010e063139b9338616ee6d0edad
alvares <alvares@chalmers.se>
2023-08-25 15:01:58 +0200

    Added remix files

---
+++ contracts/storage.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Storage {
+    struct  S {
+        int a;
+        bool b;
+    }
+
+    S s3;
+    S s4;
+    S[] s5;
+
+    constructor(){
+
+    }
+
+    function useArray() public view returns (bool, bool) {
+        S[] memory lst = new S[](10);
+        S memory s1 = lst[0];
+        S memory s2 = lst[1];
+        int a1 = s1.a;
+        int a2 = s2.a;
+        
+        bool ba1a2 = a1 == a2;
+        bool outside = s3.a == s4.a;
+        return (ba1a2, outside);
+    }
+
+    function isShallow() public pure returns (int) {
+        S[] memory lst = new S[](10);
+        lst[1] = lst[0];
+        lst[0].a = 1;
+        return lst[1].a;
+    }
+
+    // Static Array does not work in storage
+    // function isShallowStorage() public returns (int) {
+    //     s5 = new S[](10);
+    //     s5[1] = s5[0];
+    //     s5[0].a = 1;
+    //     return s5[1].a;
+    // }
+
+    // Dynamic Arrays do not work in memory
+    // function isShallowDynamic() public pure returns (int) {
+    //     S[] memory lst;
+    //     S memory s0;
+    //     lst.push(s0);
+    //     s0.a = 1;
+    //     return lst[1].a;
+    // }
+
+    function usingStorage() public returns (int, int) {
+        s5.push(s3);
+        s3.a = 1;
+        S memory s0;
+        s0.a = 2;
+        s5.push(s0);
+        s0.a = 3;
+
+        return (s5[0].a, s5[1].a);
+    }
+}
---
+++ tests/my_test.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+import "hardhat/console.sol";
+import "../contracts/storage.sol";
+
+contract BallotTest {
+
+    Storage st;
+
+    function testStorage() public {
+        st = new Storage();
+        (int v1 , int v2) = st.usingStorage();
+        Assert.equal(v1, 0, "v1 should be deep copied");
+        Assert.equal(v2, 2, "v2 should be deep copied");
+    }
+
+}
