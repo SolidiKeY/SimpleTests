3e9364dcfc84d1653f234e911724a0936380f7db
alvares <alvares@chalmers.se>
2023-11-02 14:09:02 +0100

    added more matrix tests

--- tests/my_test.sol
+++ tests/my_test.sol
@@ -34,6 +34,25 @@ contract BallotTest {
         Assert.equal(arrayValue, 1, "Should allow to write");
     }
 
+    function testDynamicVec() public {
+        int[] memory vec1;
+        int[] memory vec2;
+
+        vec2 = vec1;
+        vec1 = new int[](10);
+
+        uint sizeVec2 = vec2.length;
+        Assert.equal(sizeVec2, 0, "did not copy vec2");
+        
+        vec2 = vec1;
+        uint sizeVec2N = vec2.length;
+        Assert.equal(sizeVec2N, 10, "copied vec2");
+
+        vec1[0] = 1;
+        int vec2p0 = vec2[0];
+        Assert.equal(vec2p0, 1, "shallow embedding");
+    }
+
     function testMatrixDefaultValue() public {
         int[5][10] memory matrix;
         int v = matrix[0][0];
@@ -51,4 +70,76 @@ contract BallotTest {
 
         Assert.equal(v2, 1, "Should shallow copy");
     }
+
+    function testDynamicMatrix() public {
+        int[][] memory matrix1;
+        int[][] memory matrix2;
+
+        matrix1 = new int[][](10);
+        matrix2 = matrix1;
+
+        uint v = matrix1.length;
+        Assert.equal(v, 10, "matrix1 should initialize");
+
+        matrix2[0] = new int[](2);
+        matrix2[0][1] = 3;
+
+        uint s10 = matrix1[0].length;
+        Assert.equal(s10, 2, "matrix1 should shadow copy length");
+    
+        int v101 = matrix1[0][1];
+        Assert.equal(v101, 3, "matrix1 should shadow copy value");
+    }
+
+    function testMatrixRowCopy() public {
+        int[][5] memory matrix;
+
+        matrix[0] = new int[](10);
+        matrix[1] = matrix[0];
+
+        uint s11 = matrix[1].length;
+        Assert.equal(s11, 10, "should copy matrix");
+
+        matrix[0][0] = 2;
+        int m10 = matrix[1][0];
+        Assert.equal(m10, 2, "should shallow copy the result");
+    }
+
+    int[5][10] SFMatrix1;
+    int[5][10] SFMatrix2;
+
+    function testStorageMatrix() public {
+        int defaultValue = SFMatrix1[0][0];
+        Assert.equal(defaultValue, 0, "storage matrix should have default value");
+
+        SFMatrix1[0][0] = 1;
+
+        SFMatrix2 = SFMatrix1;
+
+        int s00 = SFMatrix2[0][0];
+        Assert.equal(s00, 1, "should copy the value");
+
+        SFMatrix1[0][1] = 2;
+        int s01 = SFMatrix2[0][1];
+        Assert.equal(s01, 0, "should deep copy");
+    }
+
+    int[][5] SFDMatrix1;
+    int[][5] SFDMatrix2;
+
+    function testStorageFixedMatrix() public {
+        SFDMatrix1[0] = new int[](10);
+        SFDMatrix2 = SFDMatrix1;
+
+        SFDMatrix1[0][0] = 1;
+        int s200 = SFDMatrix2[0][0];
+        Assert.equal(s200, 0, "storage is just deep copy");
+
+        SFDMatrix1[1] = SFDMatrix1[0];
+
+        SFDMatrix1[0][1] = 2;
+        int s111 = SFDMatrix1[1][1];
+        Assert.equal(s111, 0, "storage is just deep copy");
+    }
+
 }
