4f7efe55700c1acce06e823bf3b6890d5f3067f3
alvares <alvares@chalmers.se>
2024-10-18 16:47:02 +0200

    Added more tests to delete storage

--- tests/delete_storage_test.sol
+++ tests/delete_storage_test.sol
@@ -20,6 +20,12 @@ contract DeleteStorageTest {
     Person[] people;
     int[] v;
     Person p;
+    int vStorage;
+
+    function testStoragePath() public {
+        // int storage vStorageP = vStorage;
+        // Code does not work
+    }
 
     function testDeleteArrayStorage() public {
         v.push(1);
@@ -64,17 +70,18 @@ contract DeleteStorageTest {
 
         s.push();
         // Stores a pointer to the last array element of s.
-        uint[] storage ptr = s[s.length - 1];
+        uint[] storage ptr = s[0];
         // Add more to ptr
-        ptr.push(0x41);
+        ptr.push(41);
         // Removes the last array element of s.
         s.pop();
+        Assert.equal(ptr.length, 0, "deleted ptr");
         // Writes to the array element that is no longer within the array.
-        ptr.push(0x42);
+        ptr.push(42);
         // Adding a new element to ``s`` now will not add an empty array, but
         // will result in an array of length 1 with ``0x42`` as element.
         s.push();
-        Assert.equal(s[s.length - 1][0], 0x42, "same value as before");
+        Assert.equal(s[0][0], 42, "same value as before");
     }
 
     function testDeleteMatrix() public {
@@ -82,22 +89,41 @@ contract DeleteStorageTest {
 
         s.push();
         // Stores a pointer to the last array element of s.
-        uint[] storage ptr = s[s.length - 1];
+        uint[] storage ptr = s[0];
         // Delete s.
         delete s;
         // Writes to the array element that is no longer within the array.
-        ptr.push(0x42);
+        ptr.push(42);
         // Adding a new element to ``s`` now will not add an empty array, but
-        // will result in an array of length 1 with ``0x42`` as element.
+        // will result in an array of length 1 with 42 as element.
         s.push();
-        Assert.equal(s[s.length - 1][0], 0x42, "same value as before");
+        Assert.equal(s[0][0], 42, "same value as before");
+    }
+
+    function testRemoveMatrixNoPath() public {
+        delete s;
+
+        s.push();
+        s[0].push(41);
+        s.pop();
+        // s[0].push(42);
+        // Code does not work because it reverts
+    }
+
+    function testAddToArray() public {
+        delete v;
+
+        v.push(0x41);
+        v.pop();
+        // int u = v[0];
+        // Code does not work because index out of bound
     }
 
     function testRemoveMatrixNoPointer() public {
         delete s;
 
         s.push();
-        s[0].push(0x42);
+        s[0].push(42);
         s.pop();
         s.push();
 
@@ -115,5 +141,42 @@ contract DeleteStorageTest {
 
         Assert.equal(s[0].length, 0, "array is erased");
         Assert.equal(ptr.length, 0, "ptr should be deleted");
+    }
+
+    uint[][][] sm;
+
+    function testBigMatrix() public {
+        delete sm;
+
+        sm.push();
+        sm[0].push();
+        sm[0][0].push();
+
+        uint[] storage smP = sm[0][0];
+        sm.pop();
+        smP.push(2);
+
+        sm.push();
+        sm[0].push();
+
+        Assert.equal(sm[0][0][0], 2, "it is the same value");
+    }
+
+    function testBigMatrix2() public {
+        delete sm;
+
+        sm.push();
+        sm[0].push();
+        sm[0][0].push(1);
+
+        uint[] storage smP = sm[0][0];
+        sm.pop();
+        Assert.equal(smP.length, 0, "Deleted everything");
+        smP.push(2);
+
+        sm.push();
+        sm[0].push();
+
+        Assert.equal(sm[0][0][0], 2, "it is the same value");
     }
 }
