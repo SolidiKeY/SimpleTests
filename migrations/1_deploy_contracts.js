a8a6575fa40f82bb1eb652e1a5b1f450bc11dc13
Guilherme <alvares@chalmers.se>
2024-10-30 11:43:34 +0100

    Added the news contracts to revert

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Storage = artifacts.require("Storage");
+const RevertContract = artifacts.require("RevertContract");
 
 module.exports = function(deployer) {
   deployer.deploy(Storage);
+  deployer.deploy(RevertContract);
 };
